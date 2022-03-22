8af27e3d7c83c049b8c0da25cbe84fc475fd3c60
erankatz <eranki.katz@gmail.com>
2022-03-22 21:13:06 +0200

    das

---
+++ Notebook.cpp
@@ -0,0 +1,27 @@
+#include "Notebook.hpp"
+#include "Direction.hpp"
+using ariel::Direction;
+#include <iostream>
+#include <stdexcept>
+using namespace std;  
+using namespace ariel;
+
+Notebook::Notebook(){
+  
+}
+
+void Notebook::write(int page, int row, int col, Direction dir, string const & s){
+    
+}
+
+string Notebook::read(int page, int row, int col, Direction dir, int len){
+    return "";
+}
+
+void Notebook::erase(int page, int row, int col, Direction dir, int len){
+    
+}
+void Notebook::show(int page){
+    
+}
+
